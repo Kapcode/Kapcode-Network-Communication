48311b35783d0b0e44326f564c0cbbfdb4ae2e16
kyle <eatlinux@gmail.com>
2019-12-16 17:03:44 -0500

    -onclick, connect to selected server. -scan is not started if connected to a server on resume 'startScan()' -device MODEL is used for android systemName. -device ip address is used for scanner. -android:configChanges="keyboardHidden|orientation|screenSize" was set for MainActivity -custom ip, and usb radioButtons added to server list.

--- app/src/main/java/com/gmail/eatlinux/kapcodenetworkcommunication/MainActivity.java
+++ app/src/main/java/com/gmail/eatlinux/kapcodenetworkcommunication/MainActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.text.format.Formatter;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    final int port = 4006;
     final Handler handler=new Handler();
     MyWifiEventHandler eventHandler;
     static Thread startScannerThread;
+    static WifiClient wifiClient = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +23,18 @@ public class MainActivity extends AppCompatActivity {
         //create event handler if null
          if(eventHandler==null)eventHandler= new MyWifiEventHandler((RadioGroup)findViewById(R.id.server_list_radiogroup),handler);
     }
+
+
     public void startScan(){
         //new Thread // don't block UI thread. (Scanner process blocks.)
+        //TODO handle if Wifi is off, or not connected. turn on? wait if not connected? prompt user to connect?
         WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        final String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
         startScannerThread= new Thread((new Runnable() {
             @Override
             public void run() {
                 try{
-                    WifiScanner.startScanningIpRange("192.168.0.1",4006,127,"AndroidClient1ScannersName","demo",3000,eventHandler,true);
+                    WifiScanner.startScanningIpRange(ip,port,127,android.os.Build.MODEL+"scanner","demo",3000,eventHandler,true);
                 }catch (java.util.concurrent.RejectedExecutionException e){
                     e.printStackTrace();
                 }
@@ -42,10 +48,15 @@ public class MainActivity extends AppCompatActivity {
             }
         }));
         //if paused, un-pause
-        if(WifiScanner.executorService!=null){
-            WifiScanner.paused.set(false);
-        }else{//if null, create and start scan.
-            startScannerThread.start();
+        if(wifiClient==null) {
+            if (WifiScanner.executorService != null) {
+                WifiScanner.paused.set(false);
+            } else {//if null, create and start scan.
+                startScannerThread.start();
+            }
+        }else{
+            //make sure correct connection layout is visible.. mouse-pad, macro-pad,...where user left off, ... is connected.
+            //do not scan.
         }
 
     }
@@ -65,6 +76,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void connectButtonOnClick(View view){
         //get selected radio button, parse info, connect...
+        RadioButton rb = findViewById(((RadioGroup)findViewById(R.id.server_list_radiogroup)).getCheckedRadioButtonId());
+        if(rb == findViewById(R.id.custom_ip_radio_button)){
+            //todo make ui appear for entering ip:port...
+        }else if(rb == findViewById(R.id.usb_radio_button)){
+            //todo make usb information appear
+        }else if(rb.getText().toString().contains(":"+port)){
+            //connect to selected server
+            String[] textParts = rb.getText().toString().split(":");
+            //String serverName = textParts[0];
+            String ip = textParts[1];
+            //port is in array too, but above if statement needs it to be this.port
+            System.out.println(rb.getText().toString());
+
+            if(wifiClient==null){
+                //pause scanner
+                WifiScanner.paused.set(true);
+                //todo use application name from strings.xml
+                wifiClient = new WifiClient(ip,port,3000,android.os.Build.MODEL,"demo",false,eventHandler);
+            }
+
+        }
     }
 
 
