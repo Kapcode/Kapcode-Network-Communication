cadc25ebbd66fa5404c108d4db5a717f430a795e
kyle <eatlinux@gmail.com>
2019-12-20 17:01:17 -0500

    created and implemented startWifiWatcher(). starts in a new thread, is created only once in activity lifecycle. only created if its null.
    watches wifi state. if changes to connected, if MainActivity is visible, run startScan()...setDeviceAddress() and un-pause scanner.
    
    if changes to disconnected, pause scanner, and clear identifiedServersList.
    
    fixed bug connectBtn stayed disabled after onClick if selected radio button is usb.

--- app/src/main/java/com/gmail/eatlinux/kapcodenetworkcommunication/MainActivity.java
+++ app/src/main/java/com/gmail/eatlinux/kapcodenetworkcommunication/MainActivity.java
@@ -18,6 +18,8 @@ import android.widget.Toast;
 import com.gmail.eatlinux.kapcodenetworkcommunication.kapcode_network_universal.WifiClient;
 import com.gmail.eatlinux.kapcodenetworkcommunication.kapcode_network_universal.WifiScanner;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class MainActivity extends AppCompatActivity {
     final int port = 4006;
     static Handler handler;
@@ -27,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     static RadioGroup serverListRadioGroup = null;
     static Button connectButton;
     public static WifiScanner wifiScanner;
+    static AtomicBoolean startScanWhenWifiConnects;
+    static Thread wifiWatcherThread =null;
+    static volatile AtomicBoolean isVisible;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,15 +46,18 @@ public class MainActivity extends AppCompatActivity {
             eventHandler= new MyWifiEventHandler(serverListRadioGroup,handler);
         }
         eventHandler.setServerListRadioGroup(serverListRadioGroup);
+        if(startScanWhenWifiConnects ==null)startScanWhenWifiConnects=new AtomicBoolean(false);
+        if(isVisible==null)isVisible=new AtomicBoolean(true);
     }
 
 
-    public void startScan(){
+    public synchronized void startScan(){
         //new Thread // don't block UI thread. (Scanner process blocks.)
         // If Wi-Fi connected
         //if paused, un-pause
         if (wifiClient == null) {
             if (wifiScanner != null) {
+                //un-pause
                 wifiScanner.goal.set(WifiScanner.START);
             } else {//if null, create and start scan.
                 ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
@@ -58,15 +66,11 @@ public class MainActivity extends AppCompatActivity {
                     WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
                     final String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
                     if(wifiScanner==null){
-                        wifiScanner= new WifiScanner
-                                (android.os.Build.MODEL + "scanner"+":"+getResources().getString(R.string.app_name)+":"+ip+":"+port,
-                                        3000,128, eventHandler);
+                        wifiScanner= new WifiScanner(getDeviceAddress(), 3000,128, eventHandler);
                     }
                 }else{
                     //toast
-                    //todo set goal of scanner object to get ip and start
-                    Toast.makeText(getApplicationContext(),"WIFI IS OFF: Turn on wifi, and restart app.",Toast.LENGTH_LONG).show();
-                    this.finish();
+                    Toast.makeText(getApplicationContext(),"WIFI IS OFF: Please turn on wifi.",Toast.LENGTH_SHORT).show();
                 }
             }
         } else {
@@ -77,11 +81,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void onResume(){
+        isVisible.set(true);
         findViewById(R.id.connectButton).setEnabled(true);
         startScan();
+        startWifiWatcher();
         super.onResume();
     }
     public void onPause(){
+        isVisible.set(false);
         wifiScanner.goal.set(WifiScanner.PAUSE);
         super.onPause();
     }
@@ -91,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onDestroy(){
         //must clear list in memory, because the UI list will be cleared by super.onDestroy.
+        isVisible.set(false);
         wifiScanner.clearIdentifiedServersList();
         super.onDestroy();
     }
@@ -131,6 +139,7 @@ public class MainActivity extends AppCompatActivity {
 
 
             } else if (rb == findViewById(R.id.usb_radio_button)) {
+                connectButton.setEnabled(true);
                 //todo make usb information appear
             } else if (rb.getText().toString().contains(":" + port)) {
                 //connect to selected server
@@ -158,6 +167,60 @@ public class MainActivity extends AppCompatActivity {
         }else{
             customIP_Layout.setVisibility(View.VISIBLE);
         }
+    }
+
+
+    //watches wifi connectivity state.
+
+    //if was connected, and just disconnected -> pause scanner, clear the IdentifiedServersList
+    //if was disconnected, and just connected -> setDeviceAddress, and un-pause scanner
+    public void startWifiWatcher(){// watches wifi
+        if(wifiWatcherThread==null){
+
+            wifiWatcherThread=new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+                    boolean lastIsConnected = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnected();
+                    //check if connection changes
+                    while(true){
+                        NetworkInfo wifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+                        //if was disconnected, and just connected, and MainActivity is visible, start scan
+                        if(!lastIsConnected && wifi.isConnected() && isVisible.get()){
+                            //if scanner has already been created, must set device address in-case it has changed
+                            if(wifiScanner!=null)wifiScanner.setDeviceAddress(getDeviceAddress());
+                            startScan();
+                        }else if(lastIsConnected &! wifi.isConnected()){
+                            //if just disconnected, pause scanner, clear list
+                            if(wifiScanner!=null){
+                                wifiScanner.goal.set(WifiScanner.PAUSE);
+                                wifiScanner.clearIdentifiedServersList();
+                            }
+                        }
+                        lastIsConnected=wifi.isConnected();
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+
+
+                }
+            });
+            wifiWatcherThread.start();
+        }
+
+    }
+
+    //concat system name, application name, wifi ip address, port
+    public String getDeviceAddress(){
+        WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        String name =android.os.Build.MODEL.replace(":","");
+        String application = getResources().getString(R.string.app_name).replace(":","");
+        return name+":"+application+":"+ip+":"+port;
     }
 
 
